9dcf1cf033f7514aaf067310762f639e4d33fd66
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-06-16 17:52:53 -0400

    实现了最简单的file上传 需要完成颗粒度更细的分类 文件夹等 控制api口等等 jwt等

--- main-server/src/modules/file/file.controller.ts
+++ main-server/src/modules/file/file.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Public } from '../auth/common/auth.decorator';
@@ -11,6 +18,9 @@ export class FileController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new HttpException('请放入文件', HttpStatus.BAD_REQUEST);
+    }
     const payload = {
       filename: `${Date.now()}-${file.originalname}`,
       buffer: file.buffer.toString('base64'),
