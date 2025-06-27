52effe2398c5712f541a5e8b433d25569be7f37a
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-06-27 14:12:10 -0400

    开始设计右主界面上边的固定框，然后处理下面的navigation变化的子组件页面

--- my-next-app/src/pages/dashboard/index.jsx
+++ my-next-app/src/pages/dashboard/index.jsx
@@ -12,6 +12,7 @@ import {
   Button,
   Paper,
   Collapse,
+  Grid,
 } from "@mui/material";
 import InboxIcon from "@mui/icons-material/Inbox";
 import MailIcon from "@mui/icons-material/Mail";
@@ -19,6 +20,7 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import PagesIcon from "@mui/icons-material/Pages";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import TableChartIcon from "@mui/icons-material/TableChart";
 import "../../css/dashboard.css";
 import { useState } from "react";
 const drawerWidth = 240;
@@ -60,7 +62,7 @@ const CollapseSection = ({ title, Icon, Children }) => {
 
 export default function dashboard() {
   return (
-    <Box>
+    <Box sx={{ width: "100vw", height: "100vh", display: "flex" }}>
       <Drawer
         sx={{
           width: drawerWidth,
@@ -97,39 +99,31 @@ export default function dashboard() {
                   "Blank Page",
                 ]}
               />
+              <CollapseSection
+                title={"Tables"}
+                Icon={TableChartIcon}
+                Children={["Users", "Companies"]}
+              />
             </div>
             <div></div>
             <div></div>
           </List>
         </Paper>
-        {/* <Toolbar />
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
       </Drawer>
+      <Box sx={{ width: "100%" }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            height: "10%",
+            alignItems: "center",
+            borderBottom: "1px solid #ccc",
+          }}
+        >
+          <Grid size={8}>hello</Grid>
+          <Grid size={4}> world</Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 }
