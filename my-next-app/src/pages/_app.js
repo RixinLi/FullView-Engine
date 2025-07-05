c3a0e087096faa6c079375e56e3234084adc2ecb
Rixin Li <90631474+RixinLi@users.noreply.github.com>
2025-07-04 21:41:03 -0400

    实现card，后面重新设计+api

--- my-next-app/src/pages/_app.js
+++ my-next-app/src/pages/_app.js
@@ -1,4 +1,5 @@
 // pages/_app.js
+import "../css/defaultDashboard.css";
 export default function MyApp({ Component, pageProps }) {
   // 如果当前页面定义了 getLayout，就用它包裹页面；否则直接渲染页面
   const getLayout = Component.getLayout || ((page) => page);
--- my-next-app/src/pages/dashboard/default.jsx
+++ my-next-app/src/pages/dashboard/default.jsx
@@ -1,7 +1,47 @@
-import { Typography, Container, Grid, Divider } from "@mui/material";
+import {
+  Typography,
+  Container,
+  Grid,
+  Divider,
+  Card,
+  Paper,
+  CardContent,
+  Box,
+  Chip,
+} from "@mui/material";
 import DashboardLayout from "..";
-import "../../css/defaultDashboard.css";
 import { useState, useEffect } from "react";
+import "../../css/defaultDashboard.css";
+
+function InfoCard({ title, percentage, number, chipContent }) {
+  return (
+    <Card>
+      <CardContent className="css_cardsContent">
+        <Typography variant="h6" className="css_cardTitle">
+          {title}
+        </Typography>
+        <Typography variant="h3" className="css_cardNumber">
+          <Box sx={{ fontWeight: 400 }}>{number}</Box>
+        </Typography>
+        <Typography variant="subtitle2" className="css_cardBottom">
+          <span
+            style={{
+              color: "#4caf50",
+              backgroundColor: "rgba(76, 175, 80, 0.1)",
+              padding: "2px",
+              borderRadius: "3px",
+              marginRight: "8px",
+            }}
+          >
+            {percentage}
+          </span>{" "}
+          Since last month
+        </Typography>
+        <Chip className="css_cardChip" label={chipContent}></Chip>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function DefaultDashboard() {
   const [user, setUser] = useState(null);
@@ -12,7 +52,7 @@ export default function DefaultDashboard() {
     }
   }, []);
   return (
-    <Container className="css_container">
+    <Container className="css_topContainer">
       <Grid container>
         <Grid>
           <Typography variant="h3" className="css_typoH3">
@@ -24,6 +64,35 @@ export default function DefaultDashboard() {
         </Grid>
       </Grid>
       <Divider className="css_divider"></Divider>
+      <Grid container className="css_cardsGridContainer">
+        <InfoCard
+          title={"Sales Today"}
+          number={2.532}
+          percentage={"+26%"}
+          chipContent={"today"}
+        />
+
+        <InfoCard
+          title={"Sales Today"}
+          number={2.532}
+          percentage={"+26%"}
+          chipContent={"today"}
+        />
+
+        <InfoCard
+          title={"Sales Today"}
+          number={2.532}
+          percentage={"+26%"}
+          chipContent={"today"}
+        />
+
+        <InfoCard
+          title={"Sales Today"}
+          number={2.532}
+          percentage={"+26%"}
+          chipContent={"today"}
+        />
+      </Grid>
     </Container>
   );
 }
